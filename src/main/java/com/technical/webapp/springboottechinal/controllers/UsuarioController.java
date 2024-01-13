557bba2fac34ddbdbbbc261c6bf522f26df18159
BryanSalgadoCode <bsalgado@itstrategys.com>
2024-01-12 19:51:05 -0500

    Remove Unnecesary Imports

--- src/main/java/com/technical/webapp/springboottechinal/controllers/UsuarioController.java
+++ src/main/java/com/technical/webapp/springboottechinal/controllers/UsuarioController.java
@@ -10,18 +10,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.technical.webapp.springboottechinal.models.Entity.Empresa;
 import com.technical.webapp.springboottechinal.models.Entity.Usuario;
-import com.technical.webapp.springboottechinal.models.dto.EmpresaDto;
 import com.technical.webapp.springboottechinal.models.dto.UsuarioDto;
-import com.technical.webapp.springboottechinal.service.IEmpresa;
 import com.technical.webapp.springboottechinal.service.IUsuario;
 
 
